42f7650d4bafee134ed3973e36e68f45e1da58dd
Lmaxplay <lmaxplay@outlook.com>
2022-03-30 19:11:37 +0200

    updates!

--- src/index.ts
+++ src/index.ts
@@ -97,78 +97,6 @@ app.use(function(req, res, next) {
     }
 });
 
-//Unset the X-Powerd-By header
-app.use(function(req, res, next) {
-    res.removeHeader("X-Powered-By");
-    next();
-});
-
-// Automatically set the Content-Type header
-app.use(function(req, res, next) {
-    
-    if (req.url.endsWith(".html")) {
-        res.setHeader("Content-Type", "text/html");
-    } else if (req.url.endsWith(".css")) {
-        res.setHeader("Content-Type", "text/css");
-    } else if (req.url.endsWith(".js")) {
-        res.setHeader("Content-Type", "application/javascript");
-    } else if (req.url.endsWith(".png")) {
-        res.setHeader("Content-Type", "image/png");
-    } else if (req.url.endsWith(".jpg")) {
-        res.setHeader("Content-Type", "image/jpeg");
-    } else if (req.url.endsWith(".jpeg")) {
-        res.setHeader("Content-Type", "image/jpeg");
-    } else if (req.url.endsWith(".gif")) {
-        res.setHeader("Content-Type", "image/gif");
-    } else if (req.url.endsWith(".svg")) {
-        res.setHeader("Content-Type", "image/svg+xml");
-    } else if (req.url.endsWith(".woff")) {
-        res.setHeader("Content-Type", "application/font-woff");
-    } else if (req.url.endsWith(".woff2")) {
-        res.setHeader("Content-Type", "application/font-woff2");
-    } else if (req.url.endsWith(".ttf")) {
-        res.setHeader("Content-Type", "application/font-ttf");
-    } else if (req.url.endsWith(".eot")) {
-        res.setHeader("Content-Type", "application/vnd.ms-fontobject");
-    } else if (req.url.endsWith(".otf")) {
-        res.setHeader("Content-Type", "application/font-otf");
-    } else if (req.url.endsWith(".mp4")) {
-        res.setHeader("Content-Type", "video/mp4");
-    } else if (req.url.endsWith(".webm")) {
-        res.setHeader("Content-Type", "video/webm");
-    } else if (req.url.endsWith(".ogg")) {
-        res.setHeader("Content-Type", "video/ogg");
-    } else if (req.url.endsWith(".ogv")) {
-        res.setHeader("Content-Type", "video/ogg");
-    } else if (req.url.endsWith(".mp3")) {
-        res.setHeader("Content-Type", "audio/mpeg");
-    } else if (req.url.endsWith(".wav")) {
-        res.setHeader("Content-Type", "audio/wav");
-    } else if (req.url.endsWith(".webm")) {
-        res.setHeader("Content-Type", "audio/webm");
-    } else if (req.url.endsWith(".ogg")) {
-        res.setHeader("Content-Type", "audio/ogg");
-    } else if (req.url.endsWith(".oga")) {
-        res.setHeader("Content-Type", "audio/ogg");
-    } else if (req.url.endsWith(".mp3")) {
-        res.setHeader("Content-Type", "audio/mpeg");
-    } else if (req.url.endsWith(".m4a")) {
-        res.setHeader("Content-Type", "audio/mp4");
-    } else if (req.url.endsWith(".m4b")) {
-        res.setHeader("Content-Type", "audio/mp4");
-    } else if (req.url.endsWith(".m4p")) {
-        res.setHeader("Content-Type", "audio/mp4");
-    } else if (req.url.endsWith(".m4r")) {
-        res.setHeader("Content-Type", "audio/mp4");
-    } else if (req.url.endsWith(".m4v")) {
-        res.setHeader("Content-Type", "audio/mp4");
-    } else if (req.url.endsWith(".m4s")) {
-        res.setHeader("Content-Type", "audio/mp4");
-    } else if (req.url.endsWith(".swf")) {
-        res.setHeader("Content-Type", "application/x-shockwave-flash");
-    }
-});
-
 app.get("/", (req, res) => {
     if(fs.existsSync(path.join(path.join(__dirname, "../page/"), "index.html"))) {
         res.status(200).sendFile(path.join(path.join(__dirname, "../page/"), "index.html"));
@@ -222,3 +150,9 @@ https.createServer(
     console.log(`available at https://localhost:${PORTHTTPS}`);
     }
 );
+
+// On fatal error, print stack trace
+process.on('uncaughtException', function(err) {
+    console.log("Fatal error: " + err);
+    console.log(err.stack);
+});
